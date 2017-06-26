da87ad54bae29a7289354c467a9d67fc7b767e8e
Steph Banks <Stephibanks4.0@gmail.com>
2017-06-26 15:57:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+			string input;
+			do
+			{
+
+				Console.WriteLine("Enter length:");
+				string length = Console.ReadLine();
+				Console.WriteLine("Enter width:");
+				string width = Console.ReadLine();
+				double x = double.Parse(length);
+				double y = double.Parse(width);
+				double area = x * y;
+				Console.WriteLine(area + " is your rooms area");
+				double perimeter = ((2 * x) + (2 * y));
+				Console.WriteLine(perimeter + " is your rooms perimeter");
+
+				Console.WriteLine("Continue: y/n?");
+				input = Console.ReadLine();
+			}
+			while (input == "y");
+			
+
+
+
+
+			
+		}
+	}
+}
